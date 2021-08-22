0e1d98d37875f5bdd8081d41a0643033e65ef489
siva0542 <sivabhargavakanduru@gmail.com>
2021-08-22 00:04:31 -0700

    Calculator Operations Tested

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,17 @@
 public class Calculator {
+    public double sum(double input1, double input2) {
+        return input1+input2;
+    }
+
+    public double difference(double input1, double input2) {
+        return input1-input2;
+    }
+
+    public double product(double input1, double input2) {
+        return input1*input2;
+    }
+
+    public double division(double input1, double input2) {
+        return input1/input2;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    Calculator find = null;
+    @BeforeEach
+    void assignedObject(){
+        find = new Calculator();
+    }
+
+    @Test
+    void toCalculateSumOfTwoDoubleInputs(){
+        assertEquals(10.0,find.sum(6.0,4.0));
+    }
+
+    @Test
+    void toCalculateDifferenceBetweenTwoDoubleInputs(){
+        assertEquals(5.0,find.difference(10.0,5.0));
+    }
+
+    @Test
+    void toCalculateProductOfTwoDoubleInputs(){
+        assertEquals(10.0,find.product(5.0,2.0));
+    }
+
+    @Test
+    void toCalculateQuotientWhenDividingTwoDoubleInputs(){
+        assertEquals(2.0,find.division(4.0,2.0));
+    }
 }
